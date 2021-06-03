b291220964ebfa61cde6c7e36c262708c37c1b73
yoshiki <yoshiki0911@gmail.com>
2021-06-04 01:11:24 +0900

    add postprocess and show function

--- darkspace/core.py
+++ darkspace/core.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+import matplotlib.pyplot as plt
 import numpy as np
 import rawpy
 
@@ -10,6 +12,19 @@ class Core:
         self.raw = rawpy.imread(file_path)
         self.img: np.ndarray = self.raw.raw_image
         self.processed = self.raw.postprocess()
+        self.processed_list = [self.processed.copy()]
+
+    def show(self, figsize: Tuple[int, int] = (10, 32)):
+        fig = plt.figure(facecolor='white', figsize=figsize)
+        for idx, img in enumerate(self.processed_list):
+            ax = plt.subplot(411+idx, title=f'{idx}: ')
+            ax = plt.imshow(img)
+        return None
+
+    def postprocess(self, **kwargs):
+        self.processed = self.raw.postprocess(**kwargs)
+        self.processed_list.append(self.processed.copy())
+        return self
 
     def histogram(self):
         """
