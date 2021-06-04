7575f537dcde6aa3b08147de989b7c9ff0171567
yoshiki <yoshiki0911@gmail.com>
2021-06-04 23:00:37 +0900

    modify show function

--- darkspace/core.py
+++ darkspace/core.py
@@ -1,4 +1,3 @@
-from typing import Tuple
 import matplotlib.pyplot as plt
 import numpy as np
 import rawpy
@@ -12,19 +11,35 @@ class Core:
         self.raw = rawpy.imread(file_path)
         self.img: np.ndarray = self.raw.raw_image
         self.processed = self.raw.postprocess()
-        self.processed_list = [self.processed.copy()]
+        self.processed_list = [{}]
 
-    def show(self, figsize: Tuple[int, int] = (10, 32)):
-        fig = plt.figure(facecolor='white', figsize=figsize)
-        for idx, img in enumerate(self.processed_list):
-            ax = plt.subplot(411+idx, title=f'{idx}: ')
-            ax = plt.imshow(img)
+    def show(self, width: int = 20, height: int = 8):
+        rows = len(self.processed_list)
+        fig, ax = plt.subplots(nrows=rows, ncols=2, facecolor='white', figsize=(width, height * rows))
+
+        for idx, process_kwargs in enumerate(self.processed_list):
+            img = self.raw.postprocess(**process_kwargs).copy()
+            ax[idx, 0].set_title(f'{idx}: {process_kwargs}')
+            ax[idx, 0].imshow(img)
+            # plot color histogram
+            self._histogram(img, ax[idx, 1])
         return None
 
-    def postprocess(self, **kwargs):
-        self.processed = self.raw.postprocess(**kwargs)
-        self.processed_list.append(self.processed.copy())
+    def add_process(self, **kwargs):
+        self.processed_list.append(kwargs)
         return self
+
+    @staticmethod
+    def _histogram(img: np.ndarray, ax):
+        h_ax = np.arange(0, 256, 1)
+        hist_r, _ = np.histogram(img[:, :, 0], bins=256, range=(0, 255))
+        hist_g, _ = np.histogram(img[:, :, 1], bins=256, range=(0, 255))
+        hist_b, _ = np.histogram(img[:, :, 2], bins=256, range=(0, 255))
+
+        ax.bar(h_ax, hist_r, color='red', width=1, alpha=0.3)
+        ax.bar(h_ax, hist_g, color='green', width=1, alpha=0.3)
+        ax.bar(h_ax, hist_b, color='blue', width=1, alpha=0.3)
+        return None
 
     def histogram(self):
         """
